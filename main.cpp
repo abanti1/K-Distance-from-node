cf11b2e601721dda5d8e2563a0acfbd5631c4d23
abanti1 <106615480+abanti1@users.noreply.github.com>
2022-12-22 02:27:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 using namespace std;
 
-// A binary Tree node
 struct node
 {
 	int data;
@@ -11,61 +10,43 @@ struct node
 
 void printkdistanceNodeDown(node *root, int k)
 {
-	// Base Case
 	if (root == NULL || k < 0) return;
 
-	// If we reach a k distant node, print it
 	if (k==0)
 	{
 		cout << root->data << endl;
 		return;
 	}
 
-	// Recur for left and right subtrees
 	printkdistanceNodeDown(root->left, k-1);
 	printkdistanceNodeDown(root->right, k-1);
 }
 
-// Prints all nodes at distance k from a given target node.
-// The k distant nodes may be upward or downward. This function
-// Returns distance of root from target node, it returns -1 if target
-// node is not present in tree rooted with root.
+
 int printkdistanceNode(node* root, node* target , int k)
 {
-	// Base Case 1: If tree is empty, return -1
 	if (root == NULL) return -1;
 
-	// If target is same as root. Use the downward function
-	// to print all nodes at distance k in subtree rooted with
-	// target or root
 	if (root == target)
 	{
 		printkdistanceNodeDown(root, k);
 		return 0;
 	}
 
-	// Recur for left subtree
 	int dl = printkdistanceNode(root->left, target, k);
 
-	// Check if target node was found in left subtree
 	if (dl != -1)
 	{
-		// If root is at distance k from target, print root
-		// Note that dl is Distance of root's left child from target
+
 		if (dl + 1 == k)
 			cout << root->data << endl;
 
-		// Else go to right subtree and print all k-dl-2 distant nodes
-		// Note that the right child is 2 edges away from left child
 		else
 			printkdistanceNodeDown(root->right, k-dl-2);
 
-		// Add 1 to the distance and return value for parent calls
 		return 1 + dl;
 	}
 
-	// MIRROR OF ABOVE CODE FOR RIGHT SUBTREE
-	// Note that we reach here only when node was not found in left subtree
 	int dr = printkdistanceNode(root->right, target, k);
 	if (dr != -1)
 	{
@@ -76,11 +57,9 @@ int printkdistanceNode(node* root, node* target , int k)
 		return 1 + dr;
 	}
 
-	// If target was neither present in left nor in right subtree
 	return -1;
 }
 
-// A utility function to create a new binary tree node
 node *newnode(int data)
 {
 	node *temp = new node;
@@ -89,10 +68,8 @@ node *newnode(int data)
 	return temp;
 }
 
-// Driver program to test above functions
 int main()
 {
-	/* Let us construct the tree shown in above diagram */
 	node * root = newnode(20);
 	root->left = newnode(8);
 	root->right = newnode(22);
